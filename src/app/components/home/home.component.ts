7574e03296e0d4ed3a01254684a77e4964897aff
d3konr3kon <dilavuyo@outlook.com>
2024-10-21 01:08:43 +0200

    Added changes from the home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FooterNavComponent } from "../footer-nav/footer-nav.component";
 import { CarouselComponent } from "../carousel/carousel.component";
 import { TopnavComponent } from "../topnav/topnav.component";
+import { RouterLink } from '@angular/router';
+import { CategoriesComponent } from "../categories/categories.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterNavComponent, CarouselComponent, TopnavComponent],
+  imports: [FooterNavComponent, CarouselComponent, TopnavComponent, RouterLink, CategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
