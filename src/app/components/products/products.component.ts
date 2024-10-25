ddf36229a07bb64dc6ee50d1e8413912446086c9
d3konr3kon <dilavuyo@outlook.com>
2024-10-25 15:24:57 +0200

    Added Search Component to Products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopnavComponent } from "../topnav/topnav.component";
 import { FooterNavComponent } from "../footer-nav/footer-nav.component";
+import { SearchComponent } from "../search/search.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [TopnavComponent, FooterNavComponent],
+  imports: [TopnavComponent, FooterNavComponent, SearchComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
